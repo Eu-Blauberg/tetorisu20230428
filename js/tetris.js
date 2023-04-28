e41913497f1648ef434851d9ddaa15ecb8e12946
Eu-Blauberg <221205003@ccmailg.meijo-u.ac.jp>
2023-04-28 15:54:02 +0900

    白黒無常

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'green', 'yellow', 'red', 'lime', 'purple'
+    'black', 'white', 'black', 'white', 'black', 'white', 'black'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
